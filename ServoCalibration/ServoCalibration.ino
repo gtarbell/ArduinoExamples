3c3e7b8bacbd26f03d92f762575ac6fa04265f5e
Geddy Tarbell <gtarbell@navexglobal.com>
2017-11-05 15:44:01 -0800

    Add files via upload

---
+++ ServoCalibration/ServoCalibration.ino
@@ -0,0 +1,51 @@
+#include <Servo.h>
+
+int clawPort = 9;
+Servo myservo;
+int desiredPosition = 0; 
+int currentPosition =0;
+void setup() {
+  // put your setup code here, to run once:
+  myservo.attach(clawPort);
+  // initialize serial port and set baud rate
+  Serial.begin(9600);
+  Serial.write("input servo value between 0 and 180");
+  currentPosition = myservo.read();
+  Serial.println("current servo position: " + String(currentPosition));
+
+  Serial.println("input new servo value between 0 and 180");
+}
+
+void loop() {
+  
+
+  if(Serial.available() > 0)
+  {
+    desiredPosition = Serial.readString().toInt();
+
+    Serial.print("Heading to value: ");
+    Serial.println(desiredPosition, DEC);
+
+    if( desiredPosition > currentPosition)
+    {
+      for(int i = currentPosition; i <= desiredPosition; i++)
+      {
+        myservo.write(i);
+        delay(15);
+      }
+      currentPosition = desiredPosition;
+    }
+    else
+    {
+      for(int i = currentPosition; i >= desiredPosition; i--)
+      {
+        myservo.write(i);
+        delay(15);
+      }
+      currentPosition = desiredPosition;
+    }
+    Serial.println("done");
+    
+   
+  }
+}
---
+++ joystick/joystick.ino
@@ -0,0 +1,118 @@
+#include <Servo.h>
+
+int clawOpen =160;
+int clawClosed = 149;
+int currentClaw =0;
+
+int extendArmMin = 30;
+int extendArmMax = 175;
+int currentExtend = 0;
+
+int clawPin = 6;
+int extendPin = 9;
+int joystickPinX =0;
+int joystickPinY = 1;
+
+Servo clawServo;
+Servo extendServo;
+
+int xValue = 0;     
+int yValue = 0;        
+
+void setup() {
+  // put your setup code here, to run once:
+  clawServo.attach(clawPin);
+  extendServo.attach(extendPin);
+  // initialize serial port and set baud rate
+  Serial.begin(9600);
+  currentClaw = clawClosed;
+  clawServo.write(clawClosed);
+  currentExtend = extendArmMin;
+  extendServo.write(currentExtend);
+}
+
+void loop() {
+   
+  xValue = analogRead(joystickPinX);
+  delay(50);
+  yValue = analogRead(joystickPinY);
+
+  if(xValue < 100)
+  {
+    CloseClaw();
+  }
+  if(xValue > 1000)
+  {
+    OpenClaw();
+  }
+
+  if(yValue < 100)
+  {
+    ExtendArm();
+  }
+
+  if(yValue > 1000)
+  {
+    RetractArm();
+  }
+
+  Serial.print(xValue);
+  Serial.write ( ","); 
+  Serial.println(yValue);
+
+  //delay(50);
+  
+}
+
+void CloseClaw()
+{
+  Serial.write("closeClaw - Current clawValue: ");
+  Serial.println(String(currentClaw));
+  
+  if(currentClaw > clawClosed)
+  {
+    clawServo.write(currentClaw -1);
+    currentClaw--; 
+  }
+
+}
+
+void OpenClaw()
+{
+  Serial.write("OpenClaw - Current clawValue: ");
+  Serial.println(String(currentClaw));
+  
+  if(currentClaw < clawOpen)
+  {
+    clawServo.write(currentClaw + 1 );
+    currentClaw++; 
+  }
+
+}
+
+void ExtendArm()
+{
+  Serial.write("extend arm : ");
+  Serial.println(String(currentExtend));
+  
+  if(currentExtend < extendArmMax)
+  {
+    extendServo.write(currentExtend + 1);
+    currentExtend++; 
+  }
+
+}
+
+void RetractArm()
+{
+  Serial.write("RetractArm : ");
+  Serial.println(String(currentExtend));
+  
+  if(currentExtend > extendArmMin)
+  {
+    extendServo.write(currentExtend - 1);
+    currentExtend--; 
+  }
+}
+
+
